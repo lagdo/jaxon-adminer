d5d6bd85f622ff7c5439bbf605d4aeb470307357
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-03-17 04:30:26 +0100

    Added an alt entry point.
    It will help bootstrapping Adminer for Jaxon.

---
+++ adminer/jaxon.php
@@ -0,0 +1,14 @@
+<?php
+/** Adminer - Compact database management
+* @link https://www.adminer.org/
+* @author Jakub Vrana, https://www.vrana.cz/
+* @copyright 2007 Jakub Vrana
+* @license https://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+* @license https://www.gnu.org/licenses/gpl-2.0.html GNU General Public License, version 2 (one or other)
+*
+* Copied from index.php, just to bootstrap Adminer.
+* All outputs are disabled.
+*/
+
+include __DIR__ . "/include/bootstrap.inc.php";
+include __DIR__ . "/include/tmpfile.inc.php";
