9c5313ad826d42e2f62bba0a234890743e5612f7
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-05-05 22:49:33 +0200

    Set field names in table edit form.

--- templates/views/bootstrap/table/edit.php
+++ templates/views/bootstrap/table/edit.php
@@ -54,12 +54,12 @@
 <?php foreach($this->fields as $i => $field): ?>
         <div class="form-group">
             <div class="col-md-3 adminer-edit-column-left">
-                <input name="fields[1][field]" class="form-control" value="<?php
+                <input name="fields[<?php echo $i ?>][field]" class="form-control" value="<?php
                     echo $field['field'] ?>" data-maxlength="64" autocapitalize="off">
-                <input type="hidden" name="fields[1][orig]" value="<?php echo $field['field'] ?>">
+                <input type="hidden" name="fields[<?php echo $i ?>][orig]" value="<?php echo $field['field'] ?>">
             </div>
             <div class="col-md-2 adminer-edit-column-middle">
-                <select name="fields[1][type]" class="form-control">
+                <select name="fields[<?php echo $i ?>][type]" class="form-control">
 <?php foreach($field['_types_'] as $group => $types): ?>
                     <optgroup label="<?php echo $group ?>">
 <?php foreach($types as $type): ?>
@@ -70,11 +70,12 @@
                 </select>
             </div>
             <div class="col-md-1 adminer-edit-column-middle">
-                <input name="fields[1][length]" class="form-control<?php if($field['_length_required_']):
-                    ?> required<?php endif ?>" value="<?php echo $field['length'] ?>" size="3">
+                <input name="fields[<?php echo $i ?>][length]" class="form-control<?php
+                    if($field['_length_required_']): ?> required<?php endif ?>" value="<?php
+                    echo $field['length'] ?>" size="3">
             </div>
             <div class="col-md-2 adminer-edit-column-middle">
-                <select name="fields[1][collation]" class="form-control<?php
+                <select name="fields[<?php echo $i ?>][collation]" class="form-control<?php
                     if($field['_collation_hidden_']): ?> hidden<?php endif ?>">
                     <option value="">(<?php echo \adminer\lang('collation') ?>)</option>
 <?php foreach($this->collations as $group => $collations): ?>
@@ -87,7 +88,7 @@
 <?php endforeach ?>
                 </select>
 <?php if($this->unsigned): ?>
-                <select name="fields[1][unsigned]" class="form-control<?php
+                <select name="fields[<?php echo $i ?>][unsigned]" class="form-control<?php
                     if($field['_unsigned_hidden_']): ?> hidden<?php endif ?>">
                     <option value=""></option>
 <?php foreach($this->unsigned as $option): ?>
@@ -97,7 +98,7 @@
                 </select>
 <?php endif ?>
 <?php if(isset($field['on_update'])): ?>
-                <select name="fields[1][on_update]" class="form-control<?php
+                <select name="fields[<?php echo $i ?>][on_update]" class="form-control<?php
                     if($field['_on_update_hidden_']): ?> hidden<?php endif ?>">
                     <option value="">(<?php echo \adminer\lang('ON UPDATE') ?>)</option>
 <?php foreach($this->options['on_update'] as $group => $option): ?>
@@ -107,7 +108,7 @@
                 </select>
 <?php endif ?>
 <?php if($this->foreign_keys): ?>
-                <select name="fields[1][on_delete]" class="form-control<?php
+                <select name="fields[<?php echo $i ?>][on_delete]" class="form-control<?php
                     if($field['_on_delete_hidden_']): ?> hidden<?php endif ?>">
                     <option value="">(<?php echo \adminer\lang('ON DELETE') ?>)</option>
 <?php foreach($this->options['on_delete'] as $option): ?>
@@ -117,14 +118,15 @@
                 </select>
             </div>
             <div class="col-md-1 adminer-edit-column-null" style="padding-left:10px;padding-right:1px;">
-                <input type="checkbox" name="fields[6][null]" value="1" <?php
+                <input type="checkbox" name="fields[<?php echo $i ?>][null]" value="1" <?php
                     if($field['null']): ?>checked <?php endif ?>/>
             </div>
             <div class="col-md-1 adminer-edit-column-middle">
-                <input type="radio" name="auto_increment_col" value="6" <?php
+                <input type="radio" name="auto_increment_col" value="<?php echo $i ?>" <?php
                     if($field['auto_increment']): ?>checked <?php endif ?>/>
             </div>
-            <div class="col-md-2 adminer-edit-column-right">
+            <div class="col-md-2 adminer-edit-column-right" data-name="<?php
+                echo $field['field'] ?>" data-id="<?php echo $i ?>">
                 <button type="button" class="btn btn-primary btn-xs" id="adminer-table-add-column">
                     <span class="glyphicon glyphicon-plus" aria-hidden="true"></span>
                 </button>
