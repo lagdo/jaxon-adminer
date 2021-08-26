ad8b26d152693b528ea04e6fefbb34ae0c9029da
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2021-08-26 21:01:29 +0200

    Fixed select query with no text field.

--- src/Db/Proxy/TableSelectProxy.php
+++ src/Db/Proxy/TableSelectProxy.php
@@ -90,16 +90,14 @@ class TableSelectProxy
 
     /**
      * Print text length box in select
-     * @param string result of selectLengthProcess()
-     * @return null
+     * @param string|null result of selectLengthProcess()
+     * @return array
      */
     private function getLengthOptions($text_length)
     {
-        if($text_length === null)
-        {
-            return null;
-        }
-        return ['value' => \adminer\h($text_length)];
+        return [
+            'value' => $text_length === null ? 0 : \adminer\h($text_length),
+        ];
     }
 
     /**
